ca977c1a6db322b650d7c2d98c0f2719b9759fd6
Nuolong <nmcshea@nd.edu>
2020-12-07 21:24:32 -0500

    rm extract info

--- src/pdftool.py
+++ src/pdftool.py
@@ -3,26 +3,6 @@
 import sys
 from PyPDF2 import PdfFileReader, PdfFileWriter
 
-def extract_information(pdf_path):
-    with open(pdf_path, 'rb') as f:
-        pdf = PdfFileReader(f)
-        information = pdf.getDocumentInfo()
-        number_of_pages = pdf.getNumPages()
-
-    txt = f"""
-    Information about {pdf_path}:
-
-    Author: {information.author}
-    Creator: {information.creator}
-    Producer: {information.producer}
-    Subject: {information.subject}
-    Title: {information.title}
-    Number of pages: {number_of_pages}
-    """
-
-    print(txt)
-    return information
-
 def rotate_pages(pdf_path):
     pdf_writer = PdfFileWriter()
     pdf_reader = PdfFileReader(pdf_path)
