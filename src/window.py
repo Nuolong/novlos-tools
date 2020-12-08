483a110d0a22c983649a1686d8610ccfa542a1be
Nuolong <nmcshea@nd.edu>
2020-12-07 19:40:15 -0500

    allows for variable PDF file selection

--- src/window.py
+++ src/window.py
@@ -2,35 +2,42 @@
 
 import tkinter as tk
 from tkinter import filedialog
+from functools import partial
+#import pdftool.py
+
+FILE_COUNT = 5
+file_paths = []
+ent_file_explorer = []
+
+# window settings
+window = tk.Tk()
+window.title('Novlos Tool')
+window.config(background = "#e6e6ff")
 
 # File explorer
-def chooseFile():
+def chooseFile(i):
     filename = tk.filedialog.askopenfilename(initialdir = "~/Downloads",
                                              title = "Open Files",
                                              filetypes = (
                                              ("PDF files", "*.pdf"),
                                              ("All files", "*.*")))
     # change label to match selected file
-    lbl_file_explorer.configure(text = filename)
+    ent_file_explorer[i].configure(text = filename)
 
+# create files
+for i in range(FILE_COUNT):
+    ent_file_explorer.append(tk.Label(window,
+                                 width = 70,
+                                 fg = "blue"))
 
+    chooseFile_arg = partial(chooseFile, i)
+    btn_explore = tk.Button(window,
+                            text = "Select File",
+                            command = chooseFile_arg)
 
-window = tk.Tk()
-window.title('Novlos Tool')
-window.config(background = "#e6e6ff")
+    ent_file_explorer[i].grid(column = 0, row = i)
+    btn_explore.grid(column = 1, row = i)
 
-lbl_file_explorer = tk.Label(window,
-                             text = "Click 'Select File' to choose a file",
-                             width = 50, height = 4,
-                             fg = "blue")
-
-
-btn_explore = tk.Button(window,
-                        text = "Select File",
-                        command = chooseFile)
-
-lbl_file_explorer.grid(column = 1, row = 1)
-btn_explore.grid(column = 1, row = 2)
 
 # wait for events
 window.mainloop()
