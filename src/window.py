01f4b3cd5a031efc7146a5b78db99a3da6022ed5
Nuolong <nmcshea@nd.edu>
2020-12-09 03:57:43 -0500

    arrow / name entry / formatting

--- src/window.py
+++ src/window.py
@@ -9,10 +9,12 @@ import pdftool as pdf
 # constants
 FILE_COUNT = 5
 actions = ["Encrypt", "Decrypt", "Merge", "Slice"]
+background_clr = "#e6e6ff"
 
 # widget lists
 lbl_file_explorer = []
 btn_actions = []
+ent_file_names = []
 
 # files
 file_paths = [0] * FILE_COUNT
@@ -20,7 +22,7 @@ file_paths = [0] * FILE_COUNT
 # window settings
 window = tk.Tk()
 window.title('Novlos Tool')
-window.config(background = "#e6e6ff")
+window.config(background = background_clr)
 
 # action variable
 action_var = tk.IntVar()
@@ -50,17 +52,30 @@ def choose_action():
 
 # create file inputs
 for i in range(FILE_COUNT):
+    # file names
     lbl_file_explorer.append(tk.Label(window,
                                  width = 75,
                                  fg = "blue"))
 
+    # choose file button
     chooseFile_arg = partial(choose_file, i)
     btn_explore = tk.Button(window,
                             text = "Select File",
                             command = chooseFile_arg)
 
-    lbl_file_explorer[i].grid(column = 0, row = i, columnspan = 4)
+    # arrow appearance
+    lbl_arrow = tk.Label(window,
+                         text = " --> ",
+                         bg = background_clr)
+
+    # new filename entries
+    ent_file_names.append(tk.Entry(window,
+                                   justify = "center"))
+
+    lbl_file_explorer[i].grid(column = 0, row = i, columnspan = 4, padx = (5,0))
     btn_explore.grid(column = 4, row = i)
+    lbl_arrow.grid(column = 5, row = i)
+    ent_file_names[i].grid(column = 6, row = i, padx = (0,5))
 
 # create action buttons
 for i, action in enumerate(actions):
@@ -70,15 +85,16 @@ for i, action in enumerate(actions):
                                       pady = 5,
                                       variable = action_var,
                                       value = i))
-    btn_actions[i].grid(column = i, row = FILE_COUNT)
+    btn_actions[i].grid(column = i, row = FILE_COUNT, pady = (5, 5))
 
 # execute button
 btn_execute = tk.Button(window,
                         text = "Execute",
                         command = choose_action,
                         fg = "blue",
-                        bg = "#add8e6")
-btn_execute.grid(column = 4, row = FILE_COUNT)
+                        bg = "#add8e6",
+                        width = 25)
+btn_execute.grid(column = 4, row = FILE_COUNT, columnspan = 3)
 
 # wait for events
 window.mainloop()
